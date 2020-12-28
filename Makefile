305b44e1a6cfb118b5680f467e9c23575e316397
Jeril Sebastian <seb988@gmail.com>
2020-12-28 20:59:20 +0530

    barebones kernel in c working

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 build:
-	@nasm main.asm
-	@cp main /mnt/c/Users/Jeril/Desktop
+	@nasm -o main.bin main.asm
+	@gcc -ffreestanding -c kernel.c -o kernel.o
+	@ld -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
+	@cat main.bin kernel.bin > os-image.bin
+	@cp os-image.bin /mnt/c/Users/Jeril/Desktop
+
+clean:
+	@rm *.o *.bin
--- main.asm
+++ main.asm
@@ -1,9 +1,15 @@
 [org 0x7c00] ; bootloader offset
+    KERNEL_OFFSET equ 0x1000
+    mov [BOOT_DRIVE], dl
+
     mov bp, 0x9000 ; set the stack
     mov sp, bp
 
     mov bx, MSG_REAL_MODE
     call print ; This will be written after the BIOS messages
+    call print_nl
+
+    call load_kernel
 
     call switch_to_pm
     jmp $ ; this will actually never be executed
@@ -12,6 +18,20 @@
 %include "gdt.asm"
 %include "32-bit-print.asm"
 %include "32-bit-switch.asm"
+%include "disk-load.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
+
+    mov bx, KERNEL_OFFSET ; What address to load at
+    mov dh, 1 ; how many sectors to read
+    mov dl, [BOOT_DRIVE]
+
+    call load_disk
+    ret
 
 [bits 32]
 BEGIN_PM: ; after the switch we will get here
@@ -19,8 +39,10 @@ BEGIN_PM: ; after the switch we will get here
     call print_string_pm ; Note that this will be written at the top left corner
     jmp $
 
+BOOT_DRIVE db 0
 MSG_REAL_MODE db "Started in 16-bit real mode", 0
 MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+MSG_LOAD_KERNEL db "Loading the kernel", 0
 
 ; bootsector
 times 510-($-$$) db 0
