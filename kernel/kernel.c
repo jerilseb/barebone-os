b1a4b1e8e3053308c95e60fa2b1bd9de1e0a6c32
Jeril Sebastian <seb988@gmail.com>
2021-01-01 21:42:22 +0530

    minimal malloc added

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,6 +3,7 @@
 #include "../cpu/isr.h"
 #include "kernel.h"
 #include "../libc/string.h"
+#include "../libc/mem.h"
 
 void main() {
     isr_install();
@@ -15,7 +16,19 @@ void main() {
 void user_input(char *input) {
     if (strcmp(input, "END") == 0) {
         kprint("Stopping the CPU. Bye!\n");
-        asm volatile("hlt");
+        __asm__ __volatile__("hlt");
+    } else if (strcmp(input, "PAGE") == 0) {
+        u32 phys_addr;
+        u32 page = kmalloc(1000, 1, &phys_addr);
+        char page_str[16] = "";
+        hex_to_ascii(page, page_str);
+        char phys_str[16] = "";
+        hex_to_ascii(phys_addr, phys_str);
+        kprint("Page: ");
+        kprint(page_str);
+        kprint(", physical address: ");
+        kprint(phys_str);
+        kprint("\n");
     }
     kprint("You said: ");
     kprint(input);
